cf6e3460cf615b51cc516372426de081ab816633
verdantbreeze <chwil@Borealis.localdomain>
2022-10-23 10:32:42 -0400

    add css (center & blue bg) to text

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
+from flask import url_for
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
